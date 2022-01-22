aaa99ec96b568c10818d08c4dcf449d6283b5c89
egvimo <egvimo@gmail.com>
2022-01-22 16:45:56 +0100

    feat: add feed downloader

---
+++ scripts/feed_downloader.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import feedparser
+import requests
+import re
+
+URL = 'https://enny-und-mo.podigee.io/feed/mp3'
+
+# TODO Add test
+
+def extract_name(entry):
+    episode = entry.itunes_episode
+    name = re.sub('Enny und Mo: ', '', entry.title)
+    filename = "".join([c for c in name if c.isalpha() or c.isdigit() or c==' ']).rstrip()
+    return f"{episode} - {filename}"
+
+def extract_link(entry):
+    for link in entry.links:
+        if link.type == 'audio/mpeg':
+            return link.href
+    return None
+
+def download_file(link, name):
+    Path('out/').mkdir(parents=True, exist_ok=True)
+    response = requests.get(link)
+    with open(f"out/{name}.mp3", 'wb') as file:
+        file.write(response.content)
+
+def parse_feed():
+    feed = feedparser.parse(URL)
+
+    for entry in feed.entries:
+        link = extract_link(entry)
+        name = extract_name(entry)
+        print(name)
+        download_file(link, name)
+
+if __name__ == '__main__':
+    parse_feed()
